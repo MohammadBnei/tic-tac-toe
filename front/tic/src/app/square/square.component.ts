7286ea25b10296cf66c093b89b137bdb6a8abe7d
Mohammad Banaei <mohammad@DESKTOP-KVUJRIR.localdomain>
2020-02-17 17:59:46 +0100

    Working on css

--- front/tic/src/app/square/square.component.ts
+++ front/tic/src/app/square/square.component.ts
@@ -3,14 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button>{{ value }}</button>
+    <button nbButton *ngIf="!value" >{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'" >{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'" >{{ value }}</button>
   `,
-  styles: [
-    `button {
-      height: 100%;
-      width: 100%;
-    }`,
-  ]
+  styleUrls: ['./square.component.scss']
 })
 export class SquareComponent {
 
