6a111e380ceb4cbb7f434f74a8d2a112ff62024b
Mohammad Banaei <mohammad@DESKTOP-KVUJRIR.localdomain>
2020-02-17 15:55:03 +0100

    Created angular app in front/

---
+++ front/tic/src/app/square/square.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button>{{ value }}</button>
+  `,
+  styles: [
+    `button {
+      height: 100%;
+      width: 100%;
+    }`,
+  ]
+})
+export class SquareComponent {
+
+ @Input() value: 'X' | 'O';
+
+}
