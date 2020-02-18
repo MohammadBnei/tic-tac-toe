ff25fcdfeb1670c5d7b3eca8d0f750581a60e949
Mohammad Banaei <mohammad@DESKTOP-KVUJRIR.localdomain>
2020-02-18 12:49:44 +0100

    Removed intermediate forlder 'tic'

--- front/src/app/square/square.component.ts
+++ front/src/app/square/square.component.ts
@@ -4,13 +4,20 @@ import { Component, Input } from '@angular/core';
   selector: 'app-square',
   template: `
     <button nbButton *ngIf="!value" >{{ value }}</button>
-    <button nbButton hero status="success" *ngIf="value == 'X'" >{{ value }}</button>
-    <button nbButton hero status="info" *ngIf="value == 'O'" >{{ value }}</button>
+    <button nbButton hero status="{{this.buttonStatus}}" *ngIf="value" >{{ value }}</button>
   `,
   styleUrls: ['./square.component.scss']
 })
 export class SquareComponent {
 
  @Input() value: 'X' | 'O';
+
+ get buttonStatus() {
+   if (this.value === 'X') {
+     return 'success';
+   } else if (this.value === 'O') {
+     return 'info';
+   }
+ }
 
 }
